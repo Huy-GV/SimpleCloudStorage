1512576fa48297b4b2a5bdc72ea6657b27ca6f1b
BasBayBee <emperorhuyv@gmail.com>
2024-06-06 12:32:58 +1000

    fixed env related issues
    - removed env files from buildspec
    - added script that stores env in aws ssm and load them in appspec

---
+++ backend/aws-ssm-param-setter.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v aws)" ]; then
+  echo 'Error: AWS CLI is not installed' >&2
+  exit 1
+fi
+
+if [ $# -eq 0 ]; then
+  echo 'Error: env file not found' >&2
+  exit 1
+fi
+
+if [ ! -f $1 ]; then
+  echo 'Error: specified env file not found.' >&2
+  exit 1
+fi
+
+while IFS= read -r LINE; do
+  if [[ "$LINE" =~ ^\ *#.*$ ]] || [ -z "$LINE" ]; then
+    continue
+  fi
+
+  # Split line into key and value
+  KEY=$(echo "$LINE" | cut -d '=' -f 1)
+  VALUE=$(echo "$LINE" | cut -d '=' -f 2-)
+
+  if [[ $KEY == AWS_* ]]; then
+    KEY="_$KEY"
+  fi
+
+  aws ssm put-parameter --name "$KEY" --value "$VALUE" --type "String" --overwrite
+
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to set parameter '$KEY' in AWS SSM." >&2
+    exit 1
+  fi
+
+  echo "Parameter '$KEY' set in AWS SSM."
+done < "$1"
+
+echo "All parameters from '$1' set successfully."
--- backend/scripts/install.sh
+++ backend/scripts/install.sh
@@ -7,3 +7,16 @@ nvm install 21.6.1
 cd /home/ec2-user/app
 npm install --production
 npm install -g dotenv-cli
+
+ENV_VARIABLES_NAMES=("JWT_SECRET" "AWS_ACCESS_KEY" "AWS_SECRET_KEY" "AWS_BUCKET" "AWS_REGION" "CLIENT_URLS" "DATABASE_URL" "SERVER_PORT" "DOWNLOAD_DIR")
+
+for ENV_VARIABLE in "${ENV_VARIABLES_NAMES[@]}"; do
+    MODIFIED_ENV_VARIABLE="$ENV_VARIABLE"
+    if [[ $ENV_VARIABLE == AWS_* ]]; then
+        MODIFIED_ENV_VARIABLE="_$ENV_VARIABLE"
+    fi
+
+    ENV_VALUE=$(aws ssm get-parameter --name "$MODIFIED_ENV_VARIABLE" --query Parameter.Value --output text)
+
+    echo "$ENV_VARIABLE=$ENV_VALUE" >> /home/ec2-user/app/.env.prod.aws
+done
