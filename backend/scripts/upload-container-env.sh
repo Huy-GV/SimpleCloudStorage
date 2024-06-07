403288d39ec9cff14e207b32478da39aa3c0fac2
BasBayBee <emperorhuyv@gmail.com>
2024-06-07 21:39:32 +1000

    added script that uploads ecs env file to s3

---
+++ backend/scripts/upload-container-env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BUCKET_NAME="scs-ecs-env"
+FILE_PATH="./.env.production"
+REGION=$(aws ssm get-parameter --name "_AWS_REGION" --query Parameter.Value --output text)
+
+if aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
+    echo "Bucket exists."
+else
+    echo "Bucket does not exist. Creating bucket..."
+    aws s3api create-bucket --bucket "$BUCKET_NAME" --region "$REGION" --create-bucket-configuration LocationConstraint="$REGION"
+fi
+
+PARAMETERS=("JWT_SECRET" "AWS_ACCESS_KEY" "AWS_SECRET_KEY" "AWS_BUCKET" "AWS_REGION" "CLIENT_URLS" "DATABASE_URL" "SERVER_PORT" "DOWNLOAD_DIR" "AWS_ACCOUNT_ID" "REPOSITORY_NAME")
+for PARAM in "${PARAMETERS[@]}"; do
+    QUERY_PARAM="$PARAM"
+    if [[ "$PARAM" == AWS* ]]; then
+        QUERY_PARAM="_${PARAM}"
+    fi
+
+    PARAM_VALUE=$(aws ssm get-parameter --name "$QUERY_PARAM" --region "$REGION" --with-decryption --query "Parameter.Value" --output text)
+
+    echo "$PARAM=$PARAM_VALUE" >> "$FILE_PATH"
+done > "$FILE_PATH"
+
+echo "Uploading file..."
+aws s3 cp "$FILE_PATH" "s3://$BUCKET_NAME/"
+
+echo "Upload complete."
