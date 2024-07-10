3fc9f528fd987032c44f14a9d8e43c6298149b1f
BasBayBee <emperorhuyv@gmail.com>
2024-07-10 12:01:48 +1000

    fixed backend build
    - fixed build script using incorrect variable name
    - removed redundant echo commands from buildspec files

--- backend/scripts/upload-container-env.sh
+++ backend/scripts/upload-container-env.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 FILE_PATH="./aws.env"
 REGION_AWS=$(aws ssm get-parameter --name "REGION_AWS" --query Parameter.Value --with-decryption --output text)
-DATA_BUCKET_AWS=$(aws ssm get-parameter --name "DATA_BUCKET_AWS" --region "$REGION" --with-decryption --query "Parameter.Value" --output text)
-ENV_BUCKET_AWS=$(aws ssm get-parameter --name "ENV_BUCKET_AWS" --region "$REGION" --with-decryption --query "Parameter.Value" --output text)
+DATA_BUCKET_AWS=$(aws ssm get-parameter --name "DATA_BUCKET_AWS" --region "$REGION_AWS" --with-decryption --query "Parameter.Value" --output text)
+ENV_BUCKET_AWS=$(aws ssm get-parameter --name "ENV_BUCKET_AWS" --region "$REGION_AWS" --with-decryption --query "Parameter.Value" --output text)
 
 PARAMETERS=("JWT_SECRET" "CLIENT_URLS" "SERVER_PORT" "DOWNLOAD_DIR" "REPOSITORY_NAME")
 for PARAM in "${PARAMETERS[@]}"; do
-    PARAM_VALUE=$(aws ssm get-parameter --name "$PARAM" --region "$REGION" --with-decryption --query "Parameter.Value" --output text)
+    PARAM_VALUE=$(aws ssm get-parameter --name "$PARAM" --region "$REGION_AWS" --with-decryption --query "Parameter.Value" --output text)
 
     echo "$PARAM=$PARAM_VALUE" >> "$FILE_PATH"
 done > "$FILE_PATH"
 
-DATABASE_ENDPOINT=$(aws ssm get-parameter --name "DATABASE_ENDPOINT" --region "$REGION" --with-decryption --query "Parameter.Value" --output text)
-DATABASE_NAME=$(aws ssm get-parameter --name "DATABASE_NAME" --region "$REGION" --with-decryption --query "Parameter.Value" --output text)
-DATABASE_PASSWORD=$(aws ssm get-parameter --name "DATABASE_PASSWORD" --region "$REGION" --with-decryption --query "Parameter.Value" --output text)
-DATABASE_USER=$(aws ssm get-parameter --name "DATABASE_USER" --region "$REGION" --with-decryption --query "Parameter.Value" --output text)
+DATABASE_ENDPOINT=$(aws ssm get-parameter --name "DATABASE_ENDPOINT" --region "$REGION_AWS" --with-decryption --query "Parameter.Value" --output text)
+DATABASE_NAME=$(aws ssm get-parameter --name "DATABASE_NAME" --region "$REGION_AWS" --with-decryption --query "Parameter.Value" --output text)
+DATABASE_PASSWORD=$(aws ssm get-parameter --name "DATABASE_PASSWORD" --region "$REGION_AWS" --with-decryption --query "Parameter.Value" --output text)
+DATABASE_USER=$(aws ssm get-parameter --name "DATABASE_USER" --region "$REGION_AWS" --with-decryption --query "Parameter.Value" --output text)
 
 echo "REGION_AWS=$REGION_AWS" >> "$FILE_PATH"
 echo "DATA_BUCKET_AWS=$DATA_BUCKET_AWS" >> "$FILE_PATH"
